86c3eff5547090b3828422e03489b430b533caef
yiyun <1042559614@qq.com>
2019-09-19 20:40:15 +0800

    登录页面完善

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,9 +1,117 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
+// import {getToken} from '@/utils/cookie'
+import { UserLogin, UserRegister } from '@/services/login'
+import { connect } from 'dva'
+import './index.css'
 
-class Login extends Component {
-  render() {
-    return <div>this is login page</div>
+const Login = props => {
+  //定义state的值
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [btns, setBtns] = useState(['登入', '注册']);
+  const [registerShow, setRegisterShow] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [phone, setPhone] = useState('');
+
+
+  //定义方法
+  //获取用户名
+  let handleUsername = (e) => {
+    setUsername(e.target.value)
+  }
+  //获取密码
+  let handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+  //获取电话号码
+  let handlePhone = (e) => {
+    setPhone(e.target.value)
+  }
+  //点击登录或者注册按钮
+  let handleSubmit = async () => {
+    if (activeIndex === 0) {
+      if (username === '' && password === '') {
+        alert("用户名密码不可为空")
+      } else {
+        let result = await UserLogin({
+          "username": username,
+          "password": password
+        })
+        if (result.data.code === 1) {
+          alert(result.data.msg)
+        } else {
+          alert(result.data.msg)
+        }
+      }
+    } else {
+      if (username === '' && password === '' && phone === '') {
+        alert("用户名密码不可为空")
+      } else {
+        let result = await UserRegister({
+          "username": username,
+          "password": password,
+          "phone":phone
+        })
+        if (result.data.code === 1) {
+          alert(result.data.msg)
+        } else {
+          alert(result.data.msg)
+        }
+      }
+    }
+
+  }
+  return (<div className="container">
+    <header className="header">
+      <div>重点学生日周考成绩录入管理系统</div>
+    </header>
+    <main className="main">
+      <div className="content">
+        <div className="fromContent">
+          <div className="from-text">
+            <div><h2>网站工程</h2></div>
+            <div className="tab-list">
+              {
+                btns.map((item, index) => {
+                  return <span key={index} className={activeIndex === index ? 'tab-list-active' : null} onClick={() => {
+                    setActiveIndex(index)
+                    setRegisterShow(!registerShow)
+                  }}>{item}</span>
+                })
+              }
+            </div>
+            <div>
+              <input className="inp" value={username} type="text" placeholder="牛牛号/电话" onChange={(e) => { handleUsername(e) }} />
+            </div>
+            <div>
+              <input className="inp" value={password} type="password" placeholder="输入密码" onChange={(e) => { handlePassword(e) }} />
+            </div>
+            <div className="check">
+              {!registerShow ? <p><input type="checkbox" />两周内自动登入</p> :
+                <input className="inp" value={phone} type="text" placeholder="请输入手机号" onChange={(e) => { handlePhone(e) }} />}
+            </div>
+            <div>
+              <button onClick={handleSubmit}>{registerShow ? '注册' : '登入'}</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+  )
+}
+const mapState2Props = (store) => {
+  return {
+    
+  }
+}
+const mapDispatch2Props = (dispatch) => {
+  return {
+    // getUserLogin(params) {
+    //   dispatch({ type: 'login/login', payload: params })
+    // }
   }
 }
 
-export default Login
+export default connect(mapState2Props, mapDispatch2Props)(Login)
+
